1d49f1a7c9b919cbb7d69bda19ea1702124cea43
saidi <saidi.ahamada@gmail.com>
2022-04-07 17:57:55 +0200

    add doctest

--- library/film.py
+++ library/film.py
@@ -7,6 +7,30 @@ class ContentType(Enum):
 
 
 class Film():
+    """ 
+    >>> film = Film("Terminator 2", "1991", "John", "DVD")
+    >>> print(film)
+    Terminator 2
+    >>> film
+    ('1991', 'Terminator 2', 'John', 'DVD')
+    >>> film.title
+    'Terminator 2'
+    >>> film.type
+    'DVD'
+    >>> film.created_at
+    1991
+    >>> film.title = "Titanic"
+    >>> film.title
+    'Titanic'
+    >>> film.title = "R" 
+    Traceback (most recent call last):
+    ...
+    ValueError: Film title is too short
+    >>> film.type = "VVV" 
+    Traceback (most recent call last):
+    ...
+    ValueError: Content tyde does not exist
+    """
     def __init__(self, title, created_at, place, type):
         self._title = title
         self._created_at = int(created_at)
@@ -51,3 +75,7 @@ class Film():
             self._title = created_at
         else:
             raise ValueError("We do not sell movies released before 1950")
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
--- library/friend.py
+++ library/friend.py
@@ -1,4 +1,13 @@
 class Friend:
+    """
+    >>> friend = Friend('John', 'Terminator')
+    >>> friend.firstname
+    'John'
+    >>> friend
+    ('John', 'Terminator')
+    >>> print(friend)
+    John
+    """
     def __init__(self, firstname, film_title=''):
         self.firstname = firstname
         self.film_title = film_title
@@ -8,3 +17,7 @@ class Friend:
 
     def __str__(self):
         return f"{self.firstname}"
+
+if __name__== "__main__":
+    import doctest
+    doctest.testmod()
