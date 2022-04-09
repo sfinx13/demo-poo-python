02b0853675ec7906bf0ca4b3e07b7df5bbc5d943
saidi <saidi.ahamada@gmail.com>
2022-04-09 07:18:29 +0200

    add test

--- test_library.py
+++ test_library.py
@@ -1,6 +1,12 @@
 import unittest
 from library.film import Film
+from library.films import Films
 from library.friend import Friend
+from library.friends import Friends
+from library.friends_loader import FriendsLoader
+from library.films_loader import FilmsLoader
+from library.data import storage_films, storage_friends
+
 
 class TestModel(unittest.TestCase):
     def test_film_is_valid(self):
@@ -31,4 +37,32 @@ class TestModel(unittest.TestCase):
         with self.assertRaises(ValueError):
             film = Film()
             film.type = 'YTHG'
-    
+
+
+class TestLoader(unittest.TestCase):
+    def test_film_is_loaded(self):
+        film_loader = FilmsLoader()
+        films = film_loader.load()
+        self.assertEqual(len(films), len(storage_films))
+        self.assertTrue(all(isinstance(film, Film) for film in films))
+        self.assertEqual('Terminator 2 : Le Jugement dernier', films[9].title)
+
+    def test_friend_is_loaded(self):
+        friend_loader = FriendsLoader()
+        friends = friend_loader.load()
+        self.assertEqual(len(friends), len(storage_friends))
+        self.assertTrue(all(isinstance(friend, Friend) for friend in friends))
+        self.assertEqual('Paul', friends[0].firstname)
+
+
+class TestCollection(unittest.TestCase):
+    def test_film_is_added(self):
+        films = Films(FilmsLoader())
+        nb_films = len(films.get_films())
+        film = Film("Terminator 3", "2013", "John", "DVD")
+        films.add_film(film)
+        self.assertEqual(len(films.get_films()), nb_films + 1)
+
+    def test_friends_is_finded(self):
+        friends = Friends(FriendsLoader())
+        self.assertIsInstance(friends.find(0), Friend)
